4f1ae28f4770023b2cc574bf6a115521e6879039
David Ongaro <davidongaroh@gmil.com>
2022-06-15 17:48:17 +0300

    commit tree 7317e4f14407e220206338064478c45540a2d664

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+
+function submitData (name,email) {
+    
+
+    const configurationObject= {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body:JSON.stringify({
+            name,
+            email
+        })
+} 
+return fetch('http://localhost:3000/users', configurationObject)
+
+.then (response=>response.json())
+.then (object=> document.body.innerHTML=object['id'])
+
+
+.catch(function(error) { 
+    alert('Unauthorized Access')
+    document.body.innerHTML=error.message})
+}
